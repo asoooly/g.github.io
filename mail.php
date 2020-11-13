f86b622b6fc07f2e309260bfc4e5b4f16b4783e8
asoooly <66076915+asoooly@users.noreply.github.com>
2020-11-13 12:27:21 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,12 +12,12 @@ $mail = new PHPMailer();
 //Server settings 
 $mail->SMTPDebug = SMTP::DEBUG_SERVER;                // Enable verbose debug output 
 $mail->isSMTP();                                      // Send using SMTP 
-$mail->Host = 'smtp1.example.com';                    // Set the SMTP server to send through 
+$mail->Host = 'smtp.gmail.com';                       // Set the SMTP server to send through 
 $mail->SMTPAuth = true;                               // Enable SMTP authentication 
 $mail->Username = 'user@example.com';                 // SMTP username 
 $mail->Password = 'secret';                           // SMTP password 
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;   // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged 
-$mail->Port = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above 
+$mail->SMTPSecure = "ssl";                            // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged 
+$mail->Port = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above 
 
 //content
 $mail->isHTML(true);
